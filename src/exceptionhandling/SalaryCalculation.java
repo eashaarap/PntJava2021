95b0b59b193117450fa7a56c247f722b50d8f8a9
eashaarap <easha2272@gmail.com>
2021-01-23 17:29:18 -0500

    Updating Exception Handling

--- src/exceptionhandling/SalaryCalculation.java
+++ src/exceptionhandling/SalaryCalculation.java
@@ -1,2 +1,22 @@
-package exceptionhandling;public class SalaryCalculation {
+package exceptionhandling;
+
+public class SalaryCalculation {
+
+    public static void main(String[] args) {
+        SalaryCalculation.doDivision(20, 99);
+    }
+
+    public static int doDivision(int num1, int num2) {
+        int result = 0;
+        try {
+            result = num1 / num2;
+            System.out.println("Division value is: " + result);
+
+        } catch (Exception exception) {
+           // exception.printStackTrace();
+            System.out.println("This is arithmetic exception: Please use value greater than 0");
+        }
+        return result;
+    }
+
 }
