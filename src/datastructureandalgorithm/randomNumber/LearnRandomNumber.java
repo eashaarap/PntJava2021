82e08e5d36a0dcece51124a4f2f10ec198088c93
eashaarap <easha2272@gmail.com>
2021-01-31 20:39:58 -0500

    Learn Random Number

--- src/datastructureandalgorithm/randomNumber/LearnRandomNumber.java
+++ src/datastructureandalgorithm/randomNumber/LearnRandomNumber.java
@@ -1,2 +1,19 @@
-package datastructureandalgorithm.randomNumber;public class LearnRandomNumber {
+package datastructureandalgorithm.randomNumber;
+
+import java.util.Random;
+
+public class LearnRandomNumber {
+
+    public static void main(String[] args) {
+
+       int [] stId = new int [10];
+        Random random = new Random();
+        random.setSeed(20);
+        for(int i =0; i < stId.length; i++){
+            stId[i] = random.nextInt(400);
+            System.out.println(stId[i]);
+        }
+
+
+    }
 }
