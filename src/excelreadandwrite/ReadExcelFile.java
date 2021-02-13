c3e40d2038fd5e6310240251a30642a4e717b4bd
eashaarap <easha2272@gmail.com>
2021-02-13 17:47:16 -0500

    Learn Excel Read and Write

---
+++ src/excelreadandwrite/ReadExcelFile.java
@@ -0,0 +1,57 @@
+package excelreadandwrite;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ReadExcelFile {
+    //File location
+    public static String filePath = "../PnTSession/DataTest/pnt_class.xlsx";
+    public static void main(String[] args) {
+        ReadExcelFile.readExcelFile(filePath, 0);
+
+    }
+
+    public static void readExcelFile(String filePath, int sheetNumber) {
+        try{
+            //For InputSteam
+            FileInputStream inputStream = new FileInputStream(filePath);
+
+            Workbook workbook = new XSSFWorkbook(inputStream);
+            //Which sheet you want to read?
+            Sheet dataTypeSheet = workbook.getSheetAt(sheetNumber);
+            // will help us iterate through the rows
+            Iterator<Row> rowIterator = dataTypeSheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                Row currentRow = rowIterator.next();
+
+                // will help us iterate through the cells
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while(cellIterator.hasNext()) {//again we need to check how many values we have in the current row
+                    //will check if values are in the cell
+                    Cell currentCell = cellIterator.next();
+
+                    if (currentCell.getCellType() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + "       ");
+                    } else if (currentCell.getCellType() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + "         ");
+                    }
+                }
+                System.out.println();
+            }
+
+
+        } catch (FileNotFoundException fn) {
+            System.out.println("File Not Found");
+        } catch ( IOException io){
+            System.out.println("Reading done");
+        }
+
+    }
+}
